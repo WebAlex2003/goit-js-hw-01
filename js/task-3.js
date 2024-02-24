4704946487e3b77974831bc714083fc2c8079117
WebAlex2003 <nikolenkolesha30032003@gmail.com>
2024-02-25 00:56:53 +0200

    function-02

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,13 @@
 /* Task-3 */
 const getElementWidth = function (content, padding, border) {
-  const generalWidth = content + padding * 2 + border * 2;
+  const generalWidth =
+    Number.parseInt(content) +
+    Number.parseInt(padding) * 2 +
+    Number.parseFloat(border) * 2;
 
   return "general width of elemenst:", generalWidth;
 };
 
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
